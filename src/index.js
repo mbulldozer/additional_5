fa1e56536a20949dcfdc53a32d5b60970c7a65db
mbulldozer <mbulldozer@mail.ru>
2018-09-27 22:32:51 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let strArr = str.split("");
+
+  for (let i = 0; i < strArr.length - 1; i++){
+    const itemOne = strArr[i];
+    const itemTwo = strArr[i + 1];
+  
+    for(let j = 0; j < bracketsConfig.length; j++){
+      const contitionOne = itemOne == bracketsConfig[j][0];
+      const contitionTwo = itemTwo == bracketsConfig[j][1];
+  
+      if(contitionOne && contitionTwo){
+        strArr.splice(i, 2);
+        i = -1;
+        break;
+      }
+    }
+  }
+  return !strArr.length;
 }
